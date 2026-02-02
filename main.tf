721af21244179fd397103116894d6f41d1d9543c
rakesh <rakeshreddy@gmail.com>
2026-02-02 10:39:06 +0530

    added all

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ resource "aws_security_group" "main" {
   name        = var.sg_name
   description = var.sg_description
   vpc_id      = var.vpc_id
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
 
   tags = merge(
     var.sg_tags,
